c227e83eec56d10e36518f8bee8b98a185a3cec0
wzm <726759622@qq.com>
2017-12-13 14:56:00 +0800

    add a fragmnet

---
+++ app/src/main/java/github/wzm/com/foldingbottombar/BottomDialogFragment.java
@@ -0,0 +1,12 @@
+package github.wzm.com.foldingbottombar;
+
+/**
+ * Okline(Hangzhou)co,Ltd<br/>
+ * Author: wangzhongming<br/>
+ * Email:  wangzhongming@okline.cn</br>
+ * Date :  2017/12/13 14:55 </br>
+ * Summary:
+ */
+
+public class BottomDialogFragment {
+}
